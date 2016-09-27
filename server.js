7eaff86d270085193a2b5b73eaa4a41171186ebf
John Riepe <johnriepe@gmail.com>
2016-09-27 12:46:16 -0400

    added

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static('./public'));
 // MongoDB Configuration configuration (Change this URL to your own DB)
 // var databaseUrl = "mong....."
 
-var databaseUrl = process.env.MONGODB_URI || "mongodb://heroku_51q74xmp:2jo7tq1mdesgs20r65o2035mis@ds153745.mlab.com:53745/heroku_51q74xmp/:"
+var databaseUrl = "mongodb://heroku_51q74xmp:2jo7tq1mdesgs20r65o2035mis@ds153745.mlab.com:53745/heroku_51q74xmp/:"
 //var databaseUrl = 'nytreact'//'mongodb:
 var collections = ["articles"];
 
@@ -46,7 +46,7 @@ app.get('/', function(req, res){
 app.get('/saved', function(req, res) {
 
   // We will find all the records, sort it in descending order, then limit the records to 5
-  db.articles.find({}).sort('date', 'descending', function(err, doc){
+  db.articles.find().sort('date', 'desc', function(err, doc){
 
       if(err){
         console.log(err);
@@ -58,7 +58,7 @@ app.get('/saved', function(req, res) {
 });
 
 // This is the route we will send POST requests to save each search.
-app.post('/saved', function(req, res){
+app.post('/saved', function(req, res) {
   console.log("BODY insert: " + this.title);
 
   // Here we'll save the location based on the JSON input. 
@@ -67,9 +67,10 @@ app.post('/saved', function(req, res){
     "title": req.body.title,
     "date": req.body.pubDate,
     "url": req.body.url
-  }
+  };
 
-  }
+  
+  
   db.articles.insert(doc, function(err){
     if(err){
       console.log(err);
@@ -78,15 +79,15 @@ app.post('/saved', function(req, res){
       console.log('insert success');
       res.send("Saved Article");
     }
-  })
-});
+  });  
+})
 
 app.delete('/saved', function(req, res){
   console.log("BODY delete: " + req.body.title);
 
   // Here we'll save the location based on the JSON input. 
   // We'll use Date.now() to always get the current date time
-  db.articles.remove({"_id": this.index, function(err){
+  db.articles.remove({"_id": this.index}, function(err){
     if(err){
       console.log(err);
     }
